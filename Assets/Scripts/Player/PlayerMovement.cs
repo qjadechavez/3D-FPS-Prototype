c17212fcfe69606df8f3b6362cc1d8bb9347c8fb
Jhon Arol De Chavez <91430614+qjadechavez@users.noreply.github.com>
2025-09-19 02:42:23 +0800

    Add initial UserSettings and ignore configuration files
    - Created Search.index to define asset search parameters including roots, includes, excludes, and options.
    - Added Search.settings to configure search behavior, including provider settings and recent searches.
    - Established ignore.conf to specify files and directories to be excluded from version control, enhancing project cleanliness and efficiency.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,122 @@
+// using System.Numerics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    [Header("Jumping")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask whatsIsGround;
+    bool grounded;
+
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+        // Ground Check
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatsIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        // Drag
+        if (grounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // Jump
+        if (Input.GetKeyDown(jumpKey) && readyToJump && grounded)
+        { 
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // on ground
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // in air
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+    // Jump mechanics
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
